322508a868f28caad2df241c5adf6f3d7cf8b5fa
jiraganato <jiraganto@gmail.com>
2022-07-21 11:36:44 -0300

    modificaciones 21-7

--- Corralon/views.py
+++ Corralon/views.py
@@ -2,7 +2,6 @@ from http.client import NOT_FOUND
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from CCodigo.Corralon.models import Productos
 
 # Create your views here.
 
@@ -23,5 +22,5 @@ def buscarproducto(request):
     return render(request, "Corralon/productos.html")
 
 def productos(request):
-    productos = Productos.objects.all()
-    return render(request, "Corralon/productos.html", {'productos':productos})
+
+    return render(request, "Corralon/productos.html")
